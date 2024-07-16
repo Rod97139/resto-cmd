f21709ff2c1407528d2ccb9dfeb5d2a3efff7928
Rod97139 <fayaflame@gmail.com>
2024-07-16 16:12:39 +0200

    add supercremeux to cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,17 +1,34 @@
+import { useEffect, useState } from "react";
 import { useStore } from "react-redux";
+import { SuperCremeux } from "../../common/models";
 
 
 const Cart = () => {
     const store = useStore();
+    const [list, setList] = useState(store.getState().list);
+
+    useEffect(() => {
+        store.subscribe(() => {
+            setList(store.getState().list);
+        });
+    });
 
     return (
             <div className="Selection">
                 <h1>Vos produits selectionnés</h1>
                 {
-                    store.getState().list.map(
+                    list.map(
                         (item, index) => <span key={index} className="SelectedProduct">{item.title} {item.price}</span>
                     )
                 }
+                <div className="CartNavBar">
+                    <button onClick={() => 
+                        store.dispatch({type: "ADD_PRODUCT", payload: SuperCremeux})
+                        }
+                    >
+                        Ajouter un super crémeux
+                    </button>
+                </div>
             </div>
     );
 }
