b76071296179eeb77b04bab2ab2f8f670820648f
Rod97139 <fayaflame@gmail.com>
2024-07-17 03:39:06 +0200

    createAsyncThunk

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,22 +1,56 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getListQuantityProductPerName } from "../../app/selectors";
+import * as ProductList from '../../common/models';
+
+export const addProductThunk = createAsyncThunk( 'cart/addProductThunk' , async (product, thunkApi) => {
+    // Ajout du produit au panier via le dispatch du store
+    thunkApi.dispatch(cartSlice.actions.addProduct(product));
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const state = thunkApi.getState();
+            const numberProductPerName = getListQuantityProductPerName(state);
+                const numberForSpecialOffer = numberProductPerName.find((item) => item.title === "Poulet Croquant")?.quantity;
+              if (numberForSpecialOffer === 2) {
+               if(window.confirm("Voulez-vous ajouter une troisième fois ce produit à moitié prix ?")) {
+                        resolve();
+                    } else {
+                        reject();
+                    }
+                } else {
+                    reject();
+                }
+        }, 5000)
+      });
+})
 
 export const cartSlice = createSlice({
-    name: "list",
+    name: 'list',
     initialState: {},
     reducers: {
-        addProduct:  (currentState, action) => {
+        addProduct: (currentState, action) => {
             const listWithNewProduct = [...currentState, action.payload]
             return listWithNewProduct
-          },
+        },
         removeProduct: (currentState, action) => {
-            const list = [...currentState.list].filter((item, index) => index !== action.payload)
+            const list = [...currentState].filter(
+              (item, index) => index !== action.payload
+            )
             return list
-          },
+        },
         applyVoucher: (currentState, action) => {
             const withVoucherList = currentState.map(
-                item => item.title === 'Super Crémeux' ? ({...item, price: action.payload.price}) : item
-            )
-            return withVoucherList
-          },
-    }
-});
+    item => item.title === 'Super Crémeux' ? ({...item, price: action.payload.price}) : item
+     )
+     return withVoucherList
+    },
+  },
+  extraReducers: function(builder) {
+    builder.addCase(addProductThunk.fulfilled, (state) => {
+        const specialOffer = ProductList.PouletCroquant
+            return [...state, {...specialOffer, price:    Math.round((ProductList.PouletCroquant.price / 2) * 100) / 100}]
+  })
+    builder.addCase(addProductThunk.rejected, (state) => {
+        return [...state]
+    })
+  }
+})
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,29 +1,11 @@
 import { useDispatch } from "react-redux"
 import * as ProductList from "../../common/models"
 import ProductCard from "../../common/components/ProductCard"
-import { cartSlice } from "../cart/cartSlice"
-import { getListQuantityProductPerName } from "../../app/selectors"
+import { addProductThunk } from '../cart/cartSlice';
 
 
 const Menu = () => {
     const dispatch = useDispatch()
-    const addProductThunk = (product) => (dispatch, getState) => {
-      dispatch(cartSlice.actions.addProduct(product));
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          const state = getState();
-          const numberProductPerName = getListQuantityProductPerName(state);
-            const numberForSpecialOffer = numberProductPerName.find((item) => item.title === "Poulet Croquant")?.quantity;
-          if (numberForSpecialOffer === 2) {
-              window.confirm("Voulez-vous ajouter une troisième fois ce produit à moitié prix ?")
-  
-              const specialOffer = ProductList.PouletCroquant
-                dispatch(cartSlice.actions.addProduct({...specialOffer, price:       Math.round((ProductList.PouletCroquant.price / 2) * 100) / 100}));
-          }
-          resolve();
-        }, 1000)
-      });
-    }
     
   return (
     <div className="Menu">
